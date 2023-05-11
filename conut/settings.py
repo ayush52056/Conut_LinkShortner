8400b99d4dd60f6df4521fe84cf986406e0889d5
Ayush <you@example.com>
2023-05-11 11:26:20 +0530

    DEBUG=False

--- conut/settings.py
+++ conut/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'aad8j!qck5tw0812vssje7kxgwj8w08ua$qbz3&$$xenfq2860'
 SECRET_KEY = os.environ.get('SECRET_KEY', default='aad8j!qck5tw0812vssje7kxgwj8w08ua$qbz3&$$xenfq2860')
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
